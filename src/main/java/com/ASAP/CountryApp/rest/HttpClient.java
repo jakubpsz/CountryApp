7e8f1804efcb72fd41d3a7c7784c750a472d7642
jakubpsz <jakub.pszczolka5@gmail.com>
2021-01-09 01:11:37 +0100

    Replaced hard coded Urls for properties for all http requests

--- src/main/java/com/ASAP/CountryApp/rest/HttpClient.java
+++ src/main/java/com/ASAP/CountryApp/rest/HttpClient.java
@@ -9,7 +9,6 @@ import com.mashape.unirest.http.exceptions.UnirestException;
 import lombok.SneakyThrows;
 
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
@@ -17,27 +16,32 @@ public class HttpClient {
 
 
     public HttpResponse<JsonNode> getCityData(Country country, String cityName) throws UnirestException {
-        return Unirest.get(getProperty("cityDataMainUrl") + country.getWikiDataId() + getProperty("cityNamePrefix") + cityName + getProperty("cityType"))
-                .header("x-rapidapi-key", getProperty("geoDbApiKey"))
-                .header("x-rapidapi-host", getProperty("geoDb"))
+        return Unirest.get(getProperty("cityDataMainUrl") + country.getWikiDataId()
+                + getProperty("cityNamePrefix") + cityName + getProperty("cityType"))
+                .header(getProperty("keyHeaderName"), getProperty("rapidApiKey"))
+                .header(getProperty("hostHeaderName"), getProperty("geoDbHost"))
                 .asJson();
     }
 
     public HttpResponse<JsonNode> getWeatherData(City city) throws UnirestException {
-        return Unirest.get("https://api.openweathermap.org/data/2.5/weather?lat=" + city.getLatitude() + "&lon=" +
-                city.getLongitude() + "&appid=dfd9b909348c899e8070c1a8aa614001").asJson();
+        return Unirest.get(getProperty("weatherDataMainUrl") + city.getLatitude()
+                + getProperty("weatherLongitudePrefix") + city.getLongitude()
+                + getProperty("weatherApiKey"))
+                .asJson();
     }
 
     public HttpResponse<JsonNode> getCountryData(String name) throws UnirestException {
-        return Unirest.get("https://wft-geo-db.p.rapidapi.com/v1/geo/countries?namePrefix=" + name)
-                .header("x-rapidapi-key", "8112b02f25mshe599c7cd4323d07p14779ajsn5613d2018835")
-                .header("x-rapidapi-host", "wft-geo-db.p.rapidapi.com").asJson();
+        return Unirest.get(getProperty("countryDataMainUrl")+ name)
+                .header(getProperty("keyHeaderName"), getProperty("rapidApiKey"))
+                .header(getProperty("hostHeaderName"), getProperty("geoDbHost"))
+                .asJson();
     }
 
     public HttpResponse<String> getCurrencyExchangeData(String from, String to) throws UnirestException {
-        return Unirest.get("https://currency-exchange.p.rapidapi.com/exchange?from=" + from + "&to=" + to +"&q=1.0")
-                .header("x-rapidapi-key", "8112b02f25mshe599c7cd4323d07p14779ajsn5613d2018835")
-                .header("x-rapidapi-host", "currency-exchange.p.rapidapi.com")
+        return Unirest.get(getProperty("currencyDataMainUrl") + from + getProperty("currencyToPrefix")
+                + to + getProperty("currencyUrlClose"))
+                .header(getProperty("keyHeaderName"), getProperty("rapidApiKey"))
+                .header(getProperty("hostHeaderName"), getProperty("currencyApiHost"))
                 .asString();
     }
 
