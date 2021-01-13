a241cd571d91d97cca2256ca860741a19f1081e5
Mateusz Bilik <1matii1@gmail.com>
2021-01-13 15:09:29 +0100

    create request for country

--- src/main/java/com/ASAP/CountryApp/controller/CityController.java
+++ src/main/java/com/ASAP/CountryApp/controller/CityController.java
@@ -2,6 +2,7 @@ package com.ASAP.CountryApp.controller;
 
 import com.ASAP.CountryApp.logic.geo.City;
 import com.ASAP.CountryApp.service.CityService;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,10 +10,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class CityController {
 
     private final CityService cityService;
-    private final Logger logger = LoggerFactory.getLogger(CityController.class);
 
     public CityController(CityService cityService) {
         this.cityService = cityService;
@@ -21,11 +22,12 @@ public class CityController {
     @GetMapping("/city")
     public City getInfoForTheCity(@RequestParam(name = "country") String countryName,@RequestParam(name = "city") String cityName) {
         try {
-            logger.info("City info checked country={} city={}",countryName, cityName);
+            log.info("City info checked country={} city={}",countryName, cityName);
             return cityService.getCity(countryName, cityName);
         }catch (Exception e){
-            logger.error("Not able to get city={} info {}", cityName, e.getMessage() );
+            log.error("Not able to get city={} info {}", cityName, e.getMessage() );
             return null;
         }
     }
+
 }
