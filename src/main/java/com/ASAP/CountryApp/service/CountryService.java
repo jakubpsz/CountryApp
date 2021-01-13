a560a97ae32249a802264eaeebf8d8ea9edf00e0
jakubpsz <jakub.pszczolka5@gmail.com>
2021-01-13 15:34:38 +0100

    Clean up controller and service package

--- src/main/java/com/ASAP/CountryApp/service/CountryService.java
+++ src/main/java/com/ASAP/CountryApp/service/CountryService.java
@@ -1,21 +1,2 @@
-package com.ASAP.CountryApp.service;
-
-import com.ASAP.CountryApp.logic.geo.Country;
-import com.ASAP.CountryApp.logic.geo.CountryDataExtractor;
-import lombok.SneakyThrows;
-import org.springframework.stereotype.Service;
-
-@Service
-public class CountryService {
-
-    private CountryDataExtractor countryDataExtractor;
-
-    public CountryService(CountryDataExtractor countryDataExtractor) {
-        this.countryDataExtractor = countryDataExtractor;
-    }
-
-    @SneakyThrows
-    public Country getCountry(String countryName) {
-        return countryDataExtractor.getData(countryName);
-    }
+package com.ASAP.CountryApp.service;public interface CountryService {
 }
