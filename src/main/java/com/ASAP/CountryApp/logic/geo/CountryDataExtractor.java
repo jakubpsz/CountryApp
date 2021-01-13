621cdd503122a8f039eb8d788ec07a70c8afd915
jakubpsz <jakub.pszczolka5@gmail.com>
2021-01-13 14:27:50 +0100

    Added link to wiki in country

--- src/main/java/com/ASAP/CountryApp/logic/geo/CountryDataExtractor.java
+++ src/main/java/com/ASAP/CountryApp/logic/geo/CountryDataExtractor.java
@@ -2,14 +2,13 @@ package com.ASAP.CountryApp.logic.geo;
 
 import com.ASAP.CountryApp.logic.currency.CurrencyExchangeDataExtractor;
 import com.ASAP.CountryApp.logic.rest.HttpClient;
+import com.ASAP.CountryApp.logic.wiki_data.WikiDataExtractor;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
-
-import java.math.BigDecimal;
 
 @Component
 @ComponentScan (basePackages = "com.ASAP.CountryApp")
@@ -18,12 +17,14 @@ public class CountryDataExtractor {
     private HttpClient httpClient;
     private ResponseConverterGeo responseConverterGeo;
     private CurrencyExchangeDataExtractor currencyExchangeDataExtractor;
+    private WikiDataExtractor wikiDataExtractor;
 
     @Autowired
-    public CountryDataExtractor(HttpClient httpClient, ResponseConverterGeo responseConverterGeo, CurrencyExchangeDataExtractor currencyExchangeDataExtractor) {
+    public CountryDataExtractor(HttpClient httpClient, ResponseConverterGeo responseConverterGeo, CurrencyExchangeDataExtractor currencyExchangeDataExtractor, WikiDataExtractor wikiDataExtractor) {
         this.httpClient = httpClient;
         this.responseConverterGeo = responseConverterGeo;
         this.currencyExchangeDataExtractor = currencyExchangeDataExtractor;
+        this.wikiDataExtractor = wikiDataExtractor;
     }
 
     public CountryDataExtractor() {
@@ -34,6 +35,7 @@ public class CountryDataExtractor {
         Country country =  responseConverterGeo.convertResponseToCountry(response);
         country.setExchangeRate(Double.parseDouble(currencyExchangeDataExtractor.getData(country.getCurrency()
                 .replace("[\"", "").replace("\"]", ""), "PLN")));
+        country.setUrl(wikiDataExtractor.getData(countryName));
         return country;
     }
 }
