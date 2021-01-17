d719fcc3e3197747c6e49aee84c2bd56aa84f6e5
Mateusz Bilik <1matii1@gmail.com>
2021-01-17 20:48:42 +0100

    main page

--- src/main/java/com/ASAP/CountryApp/controller/CityController.java
+++ src/main/java/com/ASAP/CountryApp/controller/CityController.java
@@ -2,7 +2,9 @@ package com.ASAP.CountryApp.controller;
 
 import com.ASAP.CountryApp.logic.geo.City;
 import com.ASAP.CountryApp.service.CityService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,16 +14,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class CityController {
 
     private final CityService cityService;
+    private final ObjectMapper mapper;
 
-    public CityController(CityService cityService) {
+    public CityController(CityService cityService, ObjectMapper mapper) {
         this.cityService = cityService;
+        this.mapper = mapper;
     }
 
+    @CrossOrigin
     @GetMapping("/city")
-    public City getInfoForTheCity(@RequestParam(name = "country") String countryName, @RequestParam(name = "city") String cityName) {
+    public String getInfoForTheCity(@RequestParam(name = "country") String countryName,
+                                  @RequestParam(name = "city") String cityName) {
         try {
             log.info("City info checked country={} city={}", countryName, cityName);
-            return cityService.getCity(countryName, cityName);
+            return mapper.writeValueAsString(cityService.getCity(countryName, cityName));
         } catch (Exception e) {
             log.error("Not able to get city={} info {}", cityName, e.getMessage());
             return null;
--- src/page/scripts.js
+++ src/page/scripts.js
@@ -1,30 +1,137 @@
-
-// const buttonFind = document.querySelector('.btn');
 let form = document.forms['location']
 let country = form['country'];
 let city = form['city'];
+const tableBody = document.querySelector('.tbody');
 
-
-function buttonFind (event) {
+function buttonFind(event) {
     event.preventDefault();
-    console.log('user written: country: ' + country.value + " city: " + city.value );
+    console.log('user written: country: ' + country.value + " city: " + city.value);
 
     var x;
     fetch("http://localhost:8080/city?country=" + country.value.toLowerCase() + '&city=' + city.value.toLowerCase())
         .then(response => response.json())
-        .then(response => {
-            x = response;
-            response.forEach(function (element){
-                tableBody.innerHTML = tableBody.innerHTML + `
-            <tr>
-                <td>${element.name}</td>
-                <td>${element.address.city}</td>
-                <td>${element.address.zipcode}</td>
-            </tr>
-    `;
-            })
-        })
+        .then(cityInfo => {
+            console.log('Created table');
 
+            // document.getElementsByClassName("tbody").innerHTML =
+            //     cityInfo.name;
+
+            tableBody.innerHTML = `
+<div class="container">
+    <div class="row">
+        <div class="col-md-7 ">
+            <div class="panel panel-default">
+                <div class="panel-heading">  <h4 >${cityInfo.country.name}</h4></div>
+                <div class="panel-body">
+                    <div class="box box-info">
+                        <div class="box-body">
+                            <div class="col-sm-6">
+                                <div  align="center"> <img alt="User Pic" id="profile-image1" class="img-circle img-responsive">
+
+                                    <input class="hidden" type="file">
+                                    <a src="` + cityInfo.country.flag + `" >
+                                </div>
+                                <br>
+                                <!-- /input-group -->
+                            </div>
+                            <div class="col-sm-6">
+                                <h4 style="color:#00b1b1;">${cityInfo.name}</h4></span>
+                                <span>Id: ${cityInfo.country.wikiId}</span>
+                            </div>
+                            <div class="clearfix"></div>
+                            <hr style="margin:5px 0 5px 0;">
+
+                            <div class="col-sm-5 col-xs-6 tital " >Currency:</div>
+                            <div class="col-sm-7 col-xs-6 ">${cityInfo.country.currency}</div>
+                            <div class="clearfix"></div>
+                            <div class="bot-border"></div>
+
+                            <div class="col-sm-5 col-xs-6 tital " >Exchange rate:</div>
+                            <div class="col-sm-7">${cityInfo.country.exchangeRate}</div>
+                            <div class="clearfix"></div>
+                            <div class="bot-border"></div>
+
+                            <div class="col-sm-5 col-xs-6 tital " >Localization:</div>
+                            <div class="col-sm-7">${cityInfo.latitude}</div>
+                            <div class="col-sm-7">${cityInfo.longitude}</div>
+                            <div class="clearfix"></div>
+                            <div class="bot-border"></div>
+
+                            <div class="col-sm-5 col-xs-6 tital " >Relition:</div><div class="col-sm-7">Hindu</div>
+                            <!-- /.box-body -->
+                        </div>
+                        <!-- /.box -->
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <div class="container">
+    <div class="row">
+        <div class="col-md-7 ">
+            <div class="panel panel-default">
+            <div class="panel-heading">  <h4 >Weather:</h4></div>
+                <div class="panel-body">
+                    <div class="box box-info">
+                        <div class="box-body">
+
+                            <div class="col-sm-5 col-xs-6 tital " >Description:</div>
+                            <div class="col-sm-7">${cityInfo.weather.description}</div>
+                            <div class="clearfix"></div>
+                            <div class="bot-border"></div>
+
+                            <div class="col-sm-5 col-xs-6 tital " >Temperature:</div
+                            ><div class="col-sm-7">${cityInfo.weather.temperature}°C</div>
+                            <div class="clearfix"></div>
+                            <div class="bot-border"></div>
+
+                            <div class="col-sm-5 col-xs-6 tital " >Feels like:</div>
+                            <div class="col-sm-7">${cityInfo.weather.feelsLike}°C</div>
+                            <div class="clearfix"></div>
+                            <div class="bot-border"></div>
+                            
+                            <div class="col-sm-5 col-xs-6 tital " >Pressure:</div>
+                            <div class="col-sm-7">${cityInfo.weather.pressure}Pa</div>
+                            <div class="clearfix"></div>
+                            <div class="bot-border"></div>
+                            
+                            <div class="col-sm-5 col-xs-6 tital " >Wind:</div>
+                            <div class="col-sm-7">${cityInfo.weather.wind}m/s</div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+<div class="container">
+    <div class="row">
+        <div class="col-md-7 ">
+            <div class="panel panel-default">
+                        <div class="panel-heading">  <h4 >Other:</h4></div>
+                <div class="panel-body">
+                    <div class="box box-info">
+                        <div class="box-body">
+
+                            <div class="col-sm-5 col-xs-6 tital " >Id Country:</div>
+                            <div class="col-sm-7">${cityInfo.country.wikiId}</div>
+                            <div class="clearfix"></div>
+                            <div class="bot-border"></div>
+
+                            <div class="col-sm-5 col-xs-6 tital " >Id city:</div>
+                            <div class="col-sm-7">${cityInfo.id}</div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `;
+
+
+            // document.getElementById("flag").setAttribute("href", cityInfo.country.flag);
+
+        })
 };
 
 form.addEventListener('submit', buttonFind)
