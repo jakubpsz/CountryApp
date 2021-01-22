b238a1b5b749608fad7173cefd85124e5f1ca636
Mateusz Bilik <1matii1@gmail.com>
2021-01-23 00:36:54 +0100

    login

--- src/main/java/com/asap/country_app/controller/NavigatePageController.java
+++ src/main/java/com/asap/country_app/controller/NavigatePageController.java
@@ -4,10 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class MainPageController {
+public class NavigatePageController {
 
     @GetMapping("/")
     public String page() {
         return "main_page";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
     }
 }
