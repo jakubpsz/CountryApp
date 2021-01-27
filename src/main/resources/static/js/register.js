27385b1fdb5069834ba643fbce58f4d602fe4c6b
Mateusz Bilik <1matii1@gmail.com>
2021-01-27 23:30:56 +0100

    fixed log in page register

--- src/main/resources/static/js/register.js
+++ src/main/resources/static/js/register.js
@@ -1,7 +1,6 @@
 let form = document.forms['register1']
 let email = form['email'];
 let password = form['password'];
-const tableBody = document.querySelector('.tbody');
 
 function buttonFind(event) {
     event.preventDefault();
@@ -19,9 +18,9 @@ function buttonFind(event) {
         },
         body: JSON.stringify(user_register)
     })
-
-        .then(response => {
-            console.log(response.text());
+        .then(response => response.text())
+        .then(message => {
+            console.log(message);
         })
 };
 
