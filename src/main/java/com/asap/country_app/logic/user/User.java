6ef07030a78ad2037c90a53b700daaf6d3cb2696
Wojtek Zuk <wzukowski96@gmail.com>
2021-01-29 15:03:25 +0100

    ???

--- src/main/java/com/asap/country_app/logic/user/User.java
+++ src/main/java/com/asap/country_app/logic/user/User.java
@@ -1,20 +1,44 @@
 package com.asap.country_app.logic.user;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
 
-import java.util.ArrayList;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToOne;
+import java.util.List;
+import java.util.UUID;
 
-@Setter
-@Getter
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
 
-//    @Email
+
+    @Id
+    @GeneratedValue
+    @Type(type="org.hibernate.type.UUIDCharType")
+    private UUID id;
+
+
+    //    @Email
     private String email;
     private String password;
 
+    @OneToOne(mappedBy = "user")
     private UserInfo userInfo;
-    private ArrayList<Location> places;
+
+    @ManyToMany
+    private List<Location> visitedCities;
+    @ManyToMany
+    private List<Location> likedCities;
+    @ManyToMany
+    private List<Location> citiesWantedToVisit;
 
     public User(String email, String password) {
         this.email = email;
--- src/main/java/com/asap/country_app/logic/user/UserInfo.java
+++ src/main/java/com/asap/country_app/logic/user/UserInfo.java
@@ -1,11 +1,30 @@
 package com.asap.country_app.logic.user;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.Type;
 
-@Getter
-@Setter
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserInfo {
+
+    @Id
+    @GeneratedValue
+    @Type(type="org.hibernate.type.UUIDCharType")
+    private UUID id;
 
     private String nickName;
     private String name;
@@ -13,5 +32,10 @@ public class UserInfo {
     private String dateOfBirth;
     private String codeOfCountry;
     private String currency;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
 
 }
