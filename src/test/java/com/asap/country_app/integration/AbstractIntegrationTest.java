9ff73e5a487344ce20798be34a31ce9477b37932
Mateusz Bilik <1matii1@gmail.com>
2021-02-18 13:14:20 +0100

    test

---
+++ src/test/java/com/asap/country_app/integration/AbstractIntegrationTest.java
@@ -0,0 +1,12 @@
+package com.asap.country_app.integration;
+
+
+//@ActiveProfiles("test")
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+//@ContextConfiguration(initializers = AbstractIntegrationTest.DockerPostgreDataSourceInitializer.class)
+//public class AbstractIntegrationTest {
+//
+//
+//
+//}
